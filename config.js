e8414ffd1615c6b9a371bbd2009d4eb43169fad4
awvstop <awvstop@163.com>
2026-03-03 17:04:39 +0800

    Change admin password and JWT secret in config
    Updated admin password and JWT secret in config.

--- config.js
+++ config.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 module.exports = {
   admin: {
     username: process.env.ADMIN_USERNAME || 'admin',
-    password: process.env.ADMIN_PASSWORD || '123456'
+    password: process.env.ADMIN_PASSWORD || 'sadf23e12d1'
   },
   server: {
     port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || 'nav-item-jwt-secret-2024-secure-key'
+    jwtSecret: process.env.JWT_SECRET || 'nav-item-jwt-secret-23242342024-secure-key'
   }
+
 }; 
